d5262ef7e8d6a299ff18f1d1eec0bfb00c6e7f4b
ShivamAgrawal30 <shivam.agrawal1998@gmail.com>
2022-12-12 15:20:21 +0100

    updating repo

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "translatefunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "translateapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,102 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button, MenuItem, Select, TextField } from "@mui/material";
+
+import { API } from 'aws-amplify';
+
+// function App() {
+//   const [greeting, setGreeting] = useState(null);
+//   async function fetchGreeting(){
+//     const apiData = await API.get('translateapi', '/translate')
+//     setGreeting(apiData.message)
+//   }
+//   useEffect(() => {
+//     fetchGreeting()
+//   }, [])
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>{greeting}</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App() {
+  const [inputString, setInputString] = useState();
+  const [outputString, setOutputString] = useState();
+  const [srcLanguage, setSrcLanguage] = useState();
+  const [destLanguage, setDestLanguage] = useState();
+
+
+  const handleTranslateClicked = () => {
+    window.alert(inputString+' ,'+srcLanguage+' ,'+destLanguage);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json", "charset=iso-8859-1");
+
+    var raw = JSON.stringify({
+      srcLanguage,
+      destLanguage,
+      inputString
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+    };
+
+    fetch("https://svbmgct8kd.execute-api.us-east-1.amazonaws.com/dev/translate", requestOptions)
+      .then((response) => response.text())
+      .then((result) => setOutputString(result))
+      .catch((error) => setOutputString("error: " + error));
+  };
+
+  return (
+    <div className="App">
+      <TextField
+        id="input-string"
+        label="Multiline"
+        multiline
+        rows={4}
+        value={inputString}
+        onChange={(e) => setInputString(e.target.value)}
+      />
+      <Select
+        labelId="source-lang-label"
+        id="source-lang"
+        value={srcLanguage}
+        label="Source Language"
+        onChange={(e) => {
+          console.log(e.target.value);
+          setSrcLanguage(e.target.value);
+        }}
+      >
+        <MenuItem value={"en"}>English</MenuItem>
+        <MenuItem value={"de"}>German</MenuItem>
+        <MenuItem value={"fr"}>French</MenuItem>
+      </Select>
+
+      <Select
+        labelId="dest-lang-label"
+        id="dest-lang"
+        value={destLanguage}
+        label="Destination Language"
+        onChange={(e) => setDestLanguage(e.target.value)}
+      >
+        <MenuItem value={"en"}>English</MenuItem>
+        <MenuItem value={"de"}>German</MenuItem>
+        <MenuItem value={"fr"}>French</MenuItem>
+      </Select>
+      <Button variant="contained" onClick={handleTranslateClicked}>
+        Translate
+      </Button>
+      <meta charset="utf-8" />
+      <div>{outputString}</div>
+    </div>
+  );
+}
+
+export default App;
